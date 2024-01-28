06b38b4a12c646042db328c43faab2db651ec991
murilo ijanc <mbsd@m0x.ru>
2024-01-28 16:06:32 -0300

    list repositories from github based owner

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,48 @@
 use std::path::Path;
 
 use git2::Repository;
+use serde::Deserialize;
+
+pub struct Github {
+    owner: String,
+}
+
+impl Github {
+    pub fn new<S: Into<String>>(o: S) -> Self {
+        Self { owner: o.into() }
+    }
+
+    pub async fn repos(&self) -> Vec<GithubRepo> {
+        let client = reqwest::Client::new();
+        client
+            .get(format!("https://api.github.com/users/{}/repos?type=owner&sort=updated&direction=desc", self.owner))
+            .header("User-Agent", "kp-scm/0.1.0")
+            .send()
+            .await
+            .unwrap()
+            .json::<GithubRepos>()
+            .await
+            .unwrap()
+    }
+}
+
+pub type GithubRepos = Vec<GithubRepo>;
+
+#[derive(Debug, Deserialize)]
+struct Owner {
+    login: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GithubRepo {
+    name: String,
+    description: Option<String>,
+    full_name: String,
+    owner: Owner,
+    url: String,
+    git_url: String,
+    language: Option<String>,
+}
 
 pub struct GitRepo {
     repo: Repository,
@@ -37,6 +79,13 @@ impl GitRepo {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[tokio::test]
+    async fn github_repos() {
+        let github = Github::new("murilobsd");
+        let repos = github.repos().await;
+        assert!(repos.len() > 0);
+    }
 
     #[test]
     fn git_clone() {
